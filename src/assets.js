eea26f5099c93d601d373388744acbc8ef25fd34
Iman Tung <iman.t@go-jek.com>
2018-06-16 23:08:45 +0700

    [iman] assets as class

--- src/assets.js
+++ src/assets.js
@@ -1,39 +1,41 @@
-var assets = [];
-
-function addAssets(jsonFile) {
-  $.getJSON(jsonFile, function(data) {
-    assets.push(data)
-  })
-}
-
-
-function assetsPreloadSpritesheet(game) {
-  for (i = 0; i < assets.length; i++) {
-    asset = assets[i]
-    game.load.spritesheet(asset.id, 'assets/1x/' + asset.id + '.png', {
-      frameWidth: asset.width,
-      frameHeight: asset.height
-    });
+class Assets {
+  constructor(){
+    this.objs = [];
   }
-}
+  
+  add(obj){
+    this.objs.push(obj);
+  }
+  
+  loadSpritesheet(game){
+    console.log(this.objs)
+    for (var i = 0; i < this.objs.length; i++) {
+      var obj = this.objs[i]
+      game.load.spritesheet(obj.id, 'assets/1x/' + obj.id + '.png', {
+        frameWidth: obj.width,
+        frameHeight: obj.height
+      });
+    }
+  }
+  
+  animationsCreate(game){
+    for (var i = 0; i < this.objs.length; i++) {
+      var animations = this.objs[i].animations
+      var id = this.objs[i].id
 
-function assetsCreateAnimation(game) {
-  for (i = 0; i < assets.length; i++) {
-    animations = assets[i].animations
-    id = assets[i].id
-
-    for (var key in animations) {
-      if (animations.hasOwnProperty(key)) {
-        animation = animations[key]
-        game.anims.create({
-          key: id + "_" + key,
-          frames: game.anims.generateFrameNumbers(id, {
-            start: animation.frameStart,
-            end: animation.frameEnd
-          }),
-          frameRate: (animation.frameEnd - animation.frameStart) * 2.5,
-          repeat: -1
-        });
+      for (var key in animations) {
+        if (animations.hasOwnProperty(key)) {
+          var animation = animations[key]
+          game.anims.create({
+            key: id + "_" + key,
+            frames: game.anims.generateFrameNumbers(id, {
+              start: animation.frameStart,
+              end: animation.frameEnd
+            }),
+            frameRate: (animation.frameEnd - animation.frameStart) * 2.5,
+            repeat: -1
+          });
+        }
       }
     }
   }
